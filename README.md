8ffd6915f25fa9b6301025c78a8de378db6f6fd4
Aaron Renoir <aaron@fieldphone.com>
2015-11-19 21:13:24 -0800

    first commit

--- README.md
+++ README.md
@@ -1,26 +1,147 @@
-# Ember-cli-deploy-scp
+# ember-cli-deploy-ssh2
 
-This README outlines the details of collaborating on this Ember addon.
+> An ember-cli-deploy plugin to upload/activate/list versioned application file/s using ssh.
 
-## Installation
+<hr/>
+**WARNING: This plugin is only compatible with ember-cli-deploy versions >= 0.5.0**
+**WARNING: This plugin is a work in progress.**
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+<hr/>
 
-## Running
 
-* `ember server`
-* Visit your app at http://localhost:4200.
+This plugin uploads, activates and lists deployed revisions. It is different from other plugins as it works with multiple `applicationFiles`. The primary use case is being able to keep the `index.html` and `manifest.appcache` file versioned and activated together. 
 
-## Running Tests
 
-* `npm test` (Runs `ember try:testall` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
+## Quick Start
+To get up and running quickly, do the following:
 
-## Building
+- Ensure [ember-cli-deploy-build][1], [ember-cli-deploy-revision-data][3] and [ember-cli-deploy-display-revisions][4]) are installed and configured.
 
-* `ember build`
+- Install this plugin
 
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+```bash
+$ ember install ember-cli-deploy-ssh2
+```
+
+- Place the following configuration into `config/deploy.js`
+
+```javascript
+ENV['ssh2'] = {
+  host: 'webserver1.example.com',
+  username: 'production-deployer',
+  privateKeyPath: '~/.ssh/id_rsa',
+  port: 22,
+  applicationFiles: ['index.html', 'manifest.appcache'],
+  root: '/usr/local/www/my-application'
+}
+```
+
+- Run the pipeline
+
+```bash
+$ ember deploy
+```
+
+## Configuration Options
+
+### host
+  The host name or ip address of the machine to connet to.
+
+*Default:* `''`
+
+### username
+
+  The username to use to open a ssh connection.
+
+*Default:* `''`
+
+### privateKeyPath
+
+  The path to a private key to authenticate the ssh connection.
+
+*Default:*  ```'~/.ssh/id_rsa'```
+  
+### port
+  The port to connect on. 
+
+*Default:* ```['/']```
+
+### applicationFiles
+  A list of files to upload to the server. 
+
+*Default:* ```['index.html']```
+
+### root
+
+  A function or string used to determine where to upload `applicationFiles`.
+
+*Default:* ```'/usr/local/www/' + context.project.name()```
+
+### uploadDestination
+
+  A string or a function returning the path where the application files are stored.
+
+*Default:* 
+```
+function(context){
+  return path.join(this.readConfig('root'), 'revisions');
+}
+```
+
+### activationDestination
+
+  The path that the active version should be linked to. 
+
+*Default:* 
+```
+function(context) {
+  return path.join(this.readConfig('root'), 'active');
+}
+```
+
+### revisionManifest
+  
+  A string or a function returning the path where the revision manifest is located.
+
+*Default:* 
+```
+function(context) {
+  return path.join(this.readConfig('root'), 'revisions.json');
+}
+```
+
+### revisionMeta
+  A function returning a hash of meta data to include with the revision.
+
+*Default:* 
+```
+function(context) {
+  var revisionKey = this.readConfig('revisionKey');
+  var who = username.sync() + '@' + os.hostname();
+          
+  return {
+    revision: revisionKey,
+    deployer: who,
+    timestamp: new Date().getTime(),
+  }
+}
+```
+
+
+## Prerequisites
+
+The following properties are expected to be present on the deployment `context` object:
+
+- `distDir`                     (provided by [ember-cli-deploy-build][2])
+- `revisionData`                (provided by [ember-cli-deploy-revision-data][3])
+
+The following commands require:
+
+- `deploy:list`                 (provided by [ember-cli-deploy-display-revisions][4])
+
+
+
+[1]: http://ember-cli.github.io/ember-cli-deploy/plugins "Plugin Documentation"
+[2]: https://github.com/ember-cli-deploy/ember-cli-deploy-build "ember-cli-deploy-build"
+[3]: https://github.com/ember-cli-deploy/ember-cli-deploy-revision-data "ember-cli-deploy-revision-data"
+[4]: https://github.com/ember-cli-deploy/ember-cli-deploy-display-revisions "ember-cli-deploy-display-revisions"
--- index.js
+++ index.js
@@ -1,6 +1,394 @@
 /* jshint node: true */
 'use strict';
 
+var Promise          = require('ember-cli/lib/ext/promise');
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+var sshClient        = require('ssh2').Client;
+var path             = require('path');
+var fs               = require('fs');
+var os               = require('os');
+var username         = require('username');
+var lodash           = require('lodash');
+
+function upload(client, path, data) {
+  return new Promise(function (resolve, reject){
+    client.sftp(function(error, sftp) {
+      if (error) {
+        reject(error);
+      };
+      
+      var stream = sftp.createWriteStream(path);
+      
+      stream.on('error', reject);
+      stream.on('finish', resolve);
+      stream.write(data);
+      stream.end();
+    });
+  });
+};
+
+function readFile(client, path) {
+  var buffer = fs.createWriteStream('tmp/readFile');
+
+  return new Promise(function(resolve, reject) {
+    client.sftp(function(error, sftp) {
+      if (error) {
+        reject(error);
+      };
+
+      sftp.readFile(path, {}, function (error, data) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  });
+};
+
+function uploadFile(client, scr, dest) {
+  return new Promise(function(resolve, reject) {
+    client.sftp(function(error, sftp) {
+      if (error) {
+        reject(error);
+      };
+
+      sftp.fastPut(src, dest, {}, function (err) {
+        if (err) {
+          reject(err);
+        };          
+        resolve();
+      });
+    });
+  });
+}
+
+function clientExec(client, command) {
+  return new Promise(function(resolve, reject) {
+    client.exec(command, function(err, stream) {
+      if (err) {
+        reject(err);
+      };
+      resolve();
+    });
+  });
+}
+
+
+function putFile(client, src, dest) {
+  return new Promise(function(resolve, reject) {
+    var parts = dest.split('/');
+    var filename = parts.pop();
+    var destdir = parts.join('/');
+    var scpcmd  = 'mkdir -p ' + destdir;
+
+    clientExec(client, scpcmd).then(
+      function() {
+        client.sftp(function (err, sftp) {
+          if (err) {
+            reject(err);
+          }
+           
+          sftp.fastPut(src, dest, {}, function (err) {
+            if (err) {
+              reject(err);
+            };
+            resolve();
+          });
+        })
+      },
+      reject
+    );
+  });
+}
+
+
 module.exports = {
-  name: 'ember-cli-deploy-scp'
+  name: 'ember-cli-deploy-ssh2',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      __client: null,
+      defaultConfig: {
+        distDir: function(context) {
+          return context.distDir;
+        },
+        host: '',
+        username: '',
+        privateKeyPath: '~/.ssh/id_rsa',
+        port: 22,
+        applicationFiles: ['index.html'],
+        
+        root: function(context) {
+          return path.join('/usr/local/www', context.project.name());
+        },
+
+        activationDestination: function(context) {
+          var root = this.readConfig('root');
+
+          return path.join(root, 'active');
+        },
+        
+        uploadDestination: function(context){
+          var root = this.readConfig('root');
+
+          return path.join(root, 'revisions');
+        },
+
+        revisionManifest: function(context) {
+          var root = this.readConfig('root');
+
+          return path.join(root, 'revisions.json');
+        },
+
+        revisionKey: function(context) {
+          return context.commandOptions.revision || (context.revisionData && context.revisionData.revisionKey);
+        },
+
+        revisionMeta: function(context) {
+          var revisionKey = this.readConfig('revisionKey');
+          var who = username.sync() + '@' + os.hostname();
+          
+          return {
+            revision: revisionKey,
+            deployer: who,
+            timestamp: new Date().getTime(),
+          }
+        }
+      },
+
+      configure: function(context) {
+        this._super.configure.call(this, context);
+        //Object.keys(this.defaultConfig).forEach(this.applyDefaultConfigProperty.bind(this));
+        
+        var client = new sshClient();
+        var privateKeyPath = this.readConfig('privateKeyPath');
+        var privateKey;
+
+        if (privateKeyPath) {
+          privateKey = fs.readFileSync(privateKeyPath);
+        }
+        var options = {
+          host: this.readConfig('host'),
+          username: this.readConfig('username'),
+          port: this.readConfig('port'),
+          privateKey: privateKey
+        }
+
+        client.connect(options);
+
+        this.__client = client;
+        //return client;
+        return new Promise(function(resolve, reject) {
+          client.on('ready', resolve);
+        });
+      },
+
+      activate: function(context) {
+        this.log(this.readConfig('revisionKey'));
+        var _this = this;
+        var client = this.__client;
+        var redisDeployClient = this.readConfig('redisDeployClient');
+        var revisionKey = this.readConfig('revisionKey');
+        var activationDestination = this.readConfig('activationDestination');
+        var uploadDestination = path.join(this.readConfig('uploadDestination'), '/');
+        var activeRevisionPath =  path.join(uploadDestination, revisionKey, '/');
+
+        this.log('Activating revision ' + revisionKey);
+
+        
+        var linkCmd = 'ln -fs ' + activeRevisionPath + ' ' + activationDestination;
+        
+        return new Promise(function(resolve, reject) {
+          clientExec(client, linkCmd, _this).then(
+            function() {
+              _this.log('clientExeced');
+              _this.log('activate now');
+              _this._activateRevisionManifest().then(resolve, reject);
+            },
+            reject
+          );
+        });
+      },
+
+      _client: function() {
+        return this.__client;
+      },
+
+      _configureClient: function() {
+        var client = new sshClient();
+        var privateKeyPath = this.readConfig('privateKeyPath');
+        var privateKey;
+
+        if (privateKeyPath) {
+          privateKey = fs.readFileSync(privateKeyPath);
+        }
+        var options = {
+          host: this.readConfig('host'),
+          username: this.readConfig('username'),
+          port: this.readConfig('port'),
+          privateKey: privateKey
+        }
+
+        client.connect(options);
+
+        this.__client = client;
+
+        //return client;
+        return new Promise(function(resolve, reject) {
+          client.on('ready', resolve);
+        });
+        
+      },
+
+      fetchRevisions: function(context) {
+        return this._fetchRevisionManifest().then(
+          function(manifest) {
+            context.revisions = manifest;
+          },
+          function(error) {
+            this.log(error, {color: 'red'});
+          }
+        );
+      },
+
+      upload: function(context) {
+        var _this = this;
+        
+        return this._updateRevisionManifest().then(
+          function() {
+            return _this._uploadFiles();
+          },
+          function(error) {
+            this.log(error, {color: "red"})
+          }
+        );
+      },
+
+      teardown: function(context) {
+        this.log('teardown');
+        this.__client && this.__client.end();
+      },
+
+      _uploadFiles: function(context) {
+        var client = this._client();
+        var files = this.readConfig('applicationFiles');
+        var distDir = this.readConfig('distDir');
+        var revisionKey = this.readConfig('revisionKey');
+        var uploadDestination = this.readConfig('uploadDestination');
+        var destination = path.join(uploadDestination, revisionKey);
+        var _this = this;
+        
+        this.log('Uploading `applicationFiles` to ' + destination);
+
+        var uploading = new Promise(function(resolve, reject) {
+          var promises = [];
+          files.forEach(function(file) {
+            var src = path.join(distDir, file);
+            var dest = path.join(destination, file);
+
+            promises.push(putFile(client, src, dest, _this));
+          });
+
+          Promise.all(promises).then(resolve, reject);
+        });
+
+        uploading.then(
+          function() {
+            _this.log('Successfully uploaded file/s.', { color: 'green' });
+          },
+          function() {
+            _this.log('Faild to upload file/s.', { color: 'red' });
+          }
+        );
+
+        return uploading;
+      },
+
+      _activateRevisionManifest: function(context) {
+        var _this = this;
+        var client = this._client();
+        var revisionKey = this.readConfig('revisionKey');
+        var fetching = this._fetchRevisionManifest();
+        var manifestPath = this.readConfig('revisionManifest');
+
+        return new Promise(function(resolve, reject) {
+          fetching.then(
+            function(manifest) {
+              manifest = lodash.map(manifest, function(rev) {
+                if (rev.revision = revisionKey) {
+                  rev.active = true;
+                } else {
+                  delete rev['active'];
+                }
+                return rev;
+              });
+
+              _this.log(JSON.stringify(manifest));
+
+              var data = new Buffer(JSON.stringify(manifest), "utf-8");
+
+              upload(client, manifestPath, data, _this).then(resolve, reject);         
+            },
+            function(error) {
+              _this.log(error, {color: 'red'});
+              reject(error);
+            }
+          );
+        });
+      },
+
+      _updateRevisionManifest: function() {
+        var revisionKey = this.readConfig('revisionKey');
+        var revisionMeta = this.readConfig('revisionMeta');
+        var manifestPath = this.readConfig('revisionManifest');
+        var fetching = this._fetchRevisionManifest();
+        var client   = this._client();
+        var _this = this;
+
+
+        this.log(JSON.stringify(revisionMeta));
+
+        return new Promise(function(resolve, reject) {
+          fetching.then(
+            function(manifest) {
+              manifest = lodash.reject(manifest, {'revision':  revisionKey});
+              manifest.unshift(revisionMeta);
+
+              var data = new Buffer(JSON.stringify(manifest), "utf-8");
+
+              upload(client, manifestPath, data, _this).then(resolve, reject);         
+            },
+            function(error) {
+              _this.log(error, {color: 'red'});
+              reject(error);
+            }
+          );
+        });
+      },
+
+      _fetchRevisionManifest: function() {
+        var manifestPath = this.readConfig('revisionManifest');
+        var client = this._client();
+        var _this = this;
+        return new Promise(function(resolve, reject) {
+          readFile(client, manifestPath, _this).then(
+            function(manifest) {
+              resolve(JSON.parse(manifest));
+            },
+            function(error) {
+              //_this.log('no such file?', {color: 'red'});
+              //_this.log(error, {color: 'red'});
+              return resolve([]);
+            }
+          );
+        });
+      },
+
+    });
+
+    return new DeployPlugin();
+  }
 };
